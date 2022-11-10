24d60cc46c6e01484c2c6647062fb0b137d5b074
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-11-10 16:16:43 -0300

    Projeto Finalizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,99 @@
-import { Social } from '../../components/Social'
-import './home.css'
+import { useState, useEffect } from 'react'
 
-import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import './home.css'
+import { Social } from '../../components/Social'
+
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+
+import { db } from '../../services/firebaseConnection'
+import {
+  getDocs,
+  collection,
+  orderBy,
+  query,
+  doc,
+  getDoc,
+} from 'firebase/firestore'
 
 export default function Home() {
+  const [links, setLinks] = useState([])
+  const [socialLinks, setSocialLinks] = useState({})
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, 'links')
+      const queryRef = query(linksRef, orderBy('created', 'asc'))
+
+      getDocs(queryRef).then((snapshot) => {
+        let lista = []
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            name: doc.data().name,
+            url: doc.data().url,
+            bg: doc.data().bg,
+            color: doc.data().color,
+          })
+        })
+        setLinks(lista)
+      })
+    }
+    loadLinks()
+  }, [])
+
+  useEffect(() => {
+    function loadSocialLinks() {
+      const docRef = doc(db, 'social', 'link')
+
+      getDoc(docRef).then((snapshot) => {
+        if (snapshot.data() !== undefined) {
+          setSocialLinks({
+            facebook: snapshot.data().facebook,
+            instagram: snapshot.data().instagram,
+            youtube: snapshot.data().youtube,
+          })
+        }
+      })
+    }
+    loadSocialLinks()
+  }, [])
+
   return (
     <div className="home-container">
       <h1>Bruno Monteiro</h1>
       <span>Veja meus links 👇</span>
 
       <main className="links">
-        <section className="link-area">
-          <a href="#">
-            <p className="link-text">Github</p>
-          </a>
-        </section>
+        {links.map((item) => (
+          <section
+            key={item.id}
+            className="link-area"
+            style={{ backgroundColor: item.bg }}
+          >
+            <a href={item.url} target="_blank">
+              <p className="link-text" style={{ color: item.color }}>
+                {item.name}
+              </p>
+            </a>
+          </section>
+        ))}
 
-        <section className="link-area">
-          <a href="#">
-            <p className="link-text">Linkedin</p>
-          </a>
-        </section>
+        {links.length !== 0 && Object.keys(socialLinks).length > 0 && (
+          <footer>
+            <Social url={socialLinks?.facebook}>
+              <FaFacebook size={35} color="#fff" />
+            </Social>
 
-        <footer>
-          <Social url="https://www.facebook.com/">
-            <FaFacebook size={35} color="#fff" />
-          </Social>
+            <Social url={socialLinks?.instagram}>
+              <FaInstagram size={35} color="#fff" />
+            </Social>
 
-          <Social url="https://www.instagram.com/">
-            <FaInstagram size={35} color="#fff" />
-          </Social>
-
-          <Social url="https://twitter.com/">
-            <FaTwitter size={35} color="#fff" />
-          </Social>
-        </footer>
+            <Social url={socialLinks?.youtube}>
+              <FaYoutube size={35} color="#fff" />
+            </Social>
+          </footer>
+        )}
       </main>
     </div>
   )
